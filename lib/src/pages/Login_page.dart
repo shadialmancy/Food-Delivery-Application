a5f14143fc7a9c20761b21675f40749f05fac399
shadialmancy <shadi.almancy.sa@gmail.com>
2021-08-14 00:22:11 +0200

    Update Login_page.dart

--- lib/src/pages/Login_page.dart
+++ lib/src/pages/Login_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-main() => runApp(LoginPage());
+//main() => runApp(LoginPage());
 
 class LoginPage extends StatefulWidget {
   @override
