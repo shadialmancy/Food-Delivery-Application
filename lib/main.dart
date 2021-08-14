d75fcdd1e09c9e240f3021555a6b571e0b85d56f
junior ramayo <junior.ramayo@protonmail.com>
2021-08-13 20:25:44 -0500

    indentando el codigo

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: Config.data['title'].toString(),
       theme: ThemeInfo.getTheme(),
       routes: RoutesApp().getRoutes(),
