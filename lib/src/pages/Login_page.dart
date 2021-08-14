bc7e96ee9475b78d8526fec380d43810c490e086
Enekoos <iniciados@gmail.com>
2021-08-14 14:31:28 +0200

    merge conflicts

--- lib/src/pages/Login_page.dart
+++ lib/src/pages/Login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:ramayo_client_app/src/utils/my_colors.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -7,113 +8,286 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  
   bool passwordVisible = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 30),
-            Text("LYZOO", style: TextStyle(fontSize: 98, color: Colors.red, fontWeight: FontWeight.bold, letterSpacing: 2)),
-            SizedBox(height: 40),
-            Container(
-              width: 300,
-              height: 50,
-              child: TextFormField(
-                decoration: InputDecoration(
-                  fillColor: Color(0xFFE7E7E7),
-                  filled: true,
-                  prefixIcon: Icon(Icons.email, color: Colors.red),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 0, color: Color(0xFFE7E7E7)),
-                    borderRadius: BorderRadius.all(Radius.circular(50)),
-                  ),
-                  hintText: "Email",
-                ),
-              ),
+      body: Container(
+        width: double.infinity,
+        child: Stack(
+          children: <Widget>[
+
+            Positioned(
+              top: -80,
+              left: -100,
+              child: circle()
             ),
-            SizedBox(height: 20),
-            Container(
-              width: 300,
-              height: 50,
-              child: TextFormField(
-                obscureText: passwordVisible,
-                decoration: InputDecoration(
-                  fillColor: Color(0xFFE7E7E7),
-                  filled: true,
-                  prefixIcon: Icon(Icons.lock, color: Colors.red),
-                  suffixIcon: IconButton(
-                    icon: Icon(passwordVisible ? Icons.visibility : Icons.visibility_off),
-                    onPressed: () {
-                      setState(() {
-                        passwordVisible = !passwordVisible;
-                      });
-                    },
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 0, color: Color(0xFFE7E7E7)),
-                    borderRadius: BorderRadius.all(Radius.circular(50)),
-                  ),
-                  hintText: "Password",
-                ),
-              ),
+
+            Positioned(
+              top: 60,
+              left: 25,
+              child: text(),
             ),
-            SizedBox(height: 20),
-            Container(
-              width: 300,
-              height: 50,
-              child: RaisedButton(
-                onPressed: () {},
-                color: Colors.red,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(50))),
-                child: Text("Login", style: TextStyle(fontSize: 20, color: Colors.white)),
-              ),
-            ),
-            SizedBox(height: 20),
-            TextButton(
-              onPressed: () {},
-              child: Text('Forgot Password ?', style: TextStyle(color: Colors.black)),
-            ),
-            SizedBox(height: 0),
-            Container(
-              child: Row(
-                children: [
-                  Expanded(
-                      child: Container(
-                          margin: const EdgeInsets.only(left: 10.0, right: 15.0), child: Divider(indent: 30, color: Colors.black, height: 0))),
-                  Text("OR"),
-                  Expanded(
-                      child: Container(
-                          margin: const EdgeInsets.only(left: 15.0, right: 10.0), child: Divider(color: Colors.black, height: 0, endIndent: 30))),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Don\'t have an account yet?"),
-                TextButton(onPressed: () {}, child: Text("Register", style: TextStyle(color: Colors.black)))
-              ],
-            ),
-            SizedBox(height: 0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(onPressed: () {}, icon: Icon(FontAwesomeIcons.googlePlus), color: Colors.red, iconSize: 50),
-                SizedBox(width: 10),
-                IconButton(onPressed: () {}, icon: Icon(FontAwesomeIcons.facebook), color: Colors.red, iconSize: 50),
-                SizedBox(width: 10),
-                IconButton(onPressed: () {}, icon: Icon(FontAwesomeIcons.twitter), color: Colors.red, iconSize: 50)
-              ],
+
+            
+            SingleChildScrollView(
+              child: column(context)
             )
           ],
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    ));
+      ) // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
+
+
+
+  Widget column(BuildContext context){
+    return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            
+            SizedBox(height: 150),
+
+            title(),
+            
+            SizedBox(height: 130),
+            
+            inputEmail(),
+
+            SizedBox(height: 10),
+            
+            inputPassword(),
+
+            SizedBox(height: 20),
+
+
+            bottonLogin(),
+
+
+            SizedBox(height: 20),
+
+            
+            forgotPassword(),
+
+            SizedBox(height: 0),
+
+
+            orIcon(),
+            
+            register(),
+
+            SizedBox(height: 0),
+            
+            socialIcon(),
+          ],
+        );
+  }
+
+  Widget inputEmail(){
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50),
+      width: 300,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(231, 13, 50, 0.09),
+        borderRadius: BorderRadius.circular(30)
+      ),
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'Correo electronico',
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(15),
+          hintStyle: TextStyle(
+            color: Color(0xFFa7001c),
+          ),
+          prefixIcon: Icon(
+            Icons.email,
+            color: Color(0xFFE70D32),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget inputPassword(){
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50),
+      width: 300,
+      decoration: BoxDecoration(
+        color: MyColors.primaryOpacityColor,
+        borderRadius: BorderRadius.circular(30)
+      ),
+      child: TextFormField(
+       obscureText: true,
+       keyboardType: TextInputType.text,
+       decoration: InputDecoration(
+        hintText: 'Contraseña',
+        border: InputBorder.none,
+        contentPadding: EdgeInsets.all(15),
+        hintStyle: TextStyle(
+          color: MyColors.primaryColorDark
+        ),
+        prefixIcon: Icon(
+          Icons.lock,
+          color: MyColors.primaryColor
+        )
+       ),
+      ),
+    );
+  }
+
+  Widget bottonLogin(){
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+      child: ElevatedButton(
+       onPressed: (){}, 
+       child: Text('Login'),
+       style: ElevatedButton.styleFrom(
+         primary: MyColors.primaryColor,
+         shape: RoundedRectangleBorder(
+           borderRadius: BorderRadius.circular(30)
+         ),
+         padding: EdgeInsets.symmetric(vertical: 15)
+       ),
+      ),
+    );
+  }
+
+  Widget forgotPassword(){
+    return TextButton(
+      onPressed: () {},
+      child: Text(
+        'Forgot Password ?', 
+        style: TextStyle(
+          color: MyColors.primaryColor
+        ),
+      ),
+    );
+  }
+
+  Widget register(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        
+        Text(
+          "Don\'t have an account yet?",
+          style: TextStyle(
+            color: MyColors.primaryColor
+          )
+        ),
+                
+        TextButton(
+          onPressed: () {}, 
+          child: Text(
+            "Register", 
+            style: TextStyle(
+              color: MyColors.primaryColor
+            )
+          )
+        )
+      ],
+    );
+  }
+
+  Widget socialIcon(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+               
+        IconButton(
+          onPressed: () {}, 
+          icon: Icon(FontAwesomeIcons.googlePlus), color: Colors.red, iconSize: 50
+        ),
+               
+        SizedBox(width: 10),
+               
+        IconButton(
+          onPressed: () {}, 
+          icon: Icon(FontAwesomeIcons.facebook), color: Colors.red, iconSize: 50
+        ),
+               
+        SizedBox(width: 10),
+               
+        IconButton(
+          onPressed: () {}, 
+          icon: Icon(FontAwesomeIcons.twitter), color: Colors.red, iconSize: 50
+        )
+      ],
+    );
+  }
+
+  Widget title(){
+    return Text(
+      "LYZOO", 
+      style: TextStyle(
+        fontSize: 98, 
+        color: Colors.red, 
+        fontWeight: FontWeight.bold, 
+        letterSpacing: 2
+      )
+    );
+  }
+
+  Widget orIcon(){
+    return Container(
+      child: Row(
+       children: [
+                  
+        Expanded(
+         child: Container(
+          margin: const EdgeInsets.only(left: 10.0, right: 15.0), 
+          child: Divider(
+            indent: 30, 
+            color: MyColors.primaryColor, 
+            height: 0
+          )
+         )
+        ),
+
+        Text("OR"),
+
+        Expanded(
+         child: Container(
+          margin: const EdgeInsets.only(left: 15.0, right: 10.0), 
+          child: Divider(
+            color: MyColors.primaryColor, 
+            height: 0, 
+            endIndent: 30
+          )
+         )
+        ),
+       ],
+      ),
+    );
+  }
+
+  Widget circle(){
+    return Container(
+      width: 240,
+      height: 230,
+      decoration: BoxDecoration(
+        color: MyColors.primaryColor,
+        borderRadius: BorderRadius.circular(100)
+      )
+    );
+  }
+
+  Widget text(){
+    return Text(
+      'Login',
+      style: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+        fontSize: 22
+      )
+    );
+  }
+
+
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,17 +1,126 @@
 import 'package:flutter/material.dart';
-import 'package:ramayo_client_app/src/config/config.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:ramayo_client_app/src/pages/controllers/home_controller.dart';
 
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+ 
+  HomeController _controller = new HomeController();
+ 
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance!.addPostFrameCallback((timeStamp) {
+      _controller.init(context);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          Center(
-            child: Text('Hola Mundo ${Config.data['version']}'),
+      appBar: AppBar(
+           title: Center(child: Text('Delivery')),
+           elevation: 0,
+           backgroundColor: Colors.red,
+           leading: Icon(Icons.menu, color: Colors.white),
+           actions: <Widget>[
+             
+               IconButton(
+                 icon: Icon(Icons.search), 
+                 onPressed: (){},
+               )
+           ],
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 30),
+            child: Column(            
+              children: <Widget>[
+                               
+                 _card1(context),
+                 
+                 _card1(context),
+                 
+                 _card1(context),
+              ],
+            ),
           ),
-        ],
-      ),
+        ),
     );
   }
+
+ Widget _card1(BuildContext context){
+     final card = Container(      
+        child: Column(
+          children: <Widget>[
+        
+              Image.network(
+                'https://static.displate.com/857x1200/displate/2018-11-30/c3ec1197d3ad652433bbebf9dec1a7af_9793d944a67664785f7eaf6d30033180.jpg',                             
+              ),
+
+              SizedBox(height: 10),
+
+              ListTile(
+                 leading: Icon(Icons.timer),
+                 title: Text("Comida China Food Inn"),
+                 subtitle: Text("Comida estilo asiatica para comer en casa"),
+              ),
+              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                     
+                     Icon(Icons.star, color: Colors.yellow),
+                     Icon(Icons.star, color: Colors.yellow),
+                     Icon(Icons.star, color: Colors.yellow),
+                     Icon(Icons.star, color: Colors.yellow),
+                     Icon(Icons.star, color: Colors.yellow),
+
+                     SizedBox(width: 30,),
+                      
+                     FlatButton(
+                        child: Text("Cancelar"),
+                        onPressed: (){},
+                     ),
+
+                     FlatButton(
+                       child: Text("Aceptar"),
+                       onPressed: (){},
+                     ),
+                ],
+              ),
+          ],
+        ),
+     );
+    return GestureDetector(
+      onTap: _controller.goToDetallesPage,
+      child: Container(
+        margin: EdgeInsets.only(top: 30, left: 30, right: 30),
+        child: ClipRRect(
+           child: card,
+           borderRadius: BorderRadius.circular(30),
+        ),
+
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: <BoxShadow>[
+             BoxShadow(
+               color: Colors.black26,
+               blurRadius: 10,
+               spreadRadius: 2,
+               offset: Offset(2, 10),
+             ),
+          ],
+        ),
+      ),
+    );
+ }
+
+
 }
