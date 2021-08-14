24f8030177006b72f92d6269e8c726d2cfadf743
Enekoos <iniciados@gmail.com>
2021-08-14 14:21:03 +0200

    push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,28 @@ import 'package:ramayo_client_app/src/config/config.dart';
 import 'package:ramayo_client_app/src/config/routes.dart';
 import 'package:ramayo_client_app/src/config/theme.dart';
 import 'package:ramayo_client_app/src/services/local_notification.dart';
+import 'package:ramayo_client_app/src/services/push_notification.dart';
 import 'package:ramayo_client_app/src/services/version_app.dart';
 
 LocalNotification ln = LocalNotification();
+PushNotification pn = PushNotification();
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+    
   await VersionApp.getVersion();
+
   ln.initializeSetting();
+  pn.init();
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    pn.initListeners(context);
+
     return MaterialApp(
       title: Config.data['title'].toString(),
       theme: ThemeInfo.getTheme(),
---
+++ lib/src/services/push_notification.dart
@@ -0,0 +1,76 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel',
+  'High Importance Notifications',
+  'This channel is used for important notifications.',
+  importance: Importance.high,
+  playSound: true,
+);
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
+
+class PushNotification {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  init() async {
+    await Firebase.initializeApp();
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+    FirebaseMessaging.instance.subscribeToTopic('todos');
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+  }
+
+  initListeners(BuildContext context) {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('mensaje no in app');
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+                android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              channel.description,
+              color: Colors.blue,
+              playSound: true,
+              icon: '@mipmap/ic_launcher',
+            )));
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('mensaje in app');
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null) {
+        showDialog(
+            context: context,
+            builder: (_) {
+              return AlertDialog(
+                title: Text('${notification.title}'),
+                content: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('${notification.body}'),
+                    ],
+                  ),
+                ),
+              );
+            });
+      }
+    });
+  }
+}
