5f295f137ee6047ff858c0ef0c465eb9b16a3186
junior ramayo <junior.ramayo@protonmail.com>
2021-08-17 13:35:12 -0500

    commit

--- lib/src/config/routes.dart
+++ lib/src/config/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ramayo_client_app/src/pages/agregar_tarjeta.dart';
 import 'package:ramayo_client_app/src/pages/detalle_page.dart';
 import 'package:ramayo_client_app/src/pages/home_page.dart';
+import 'package:ramayo_client_app/src/pages/information_restaurant_page.dart';
 import 'package:ramayo_client_app/src/pages/login_page.dart';
 import 'package:ramayo_client_app/src/pages/pruebas_page.dart';
 import 'package:ramayo_client_app/src/pages/registration/activar_ubicacion_page.dart';
@@ -27,6 +28,7 @@ class RoutesApp {
       '/registerPassword': (BuildContext context) => RegisterPassword(),
       '/registerNameYLastname': (BuildContext context) => RegisterNameYLastName(),
       '/ubication': (BuildContext context) => ActivarUbicacion(),
+      '/informationRestaurant': (BuildContext context) => InformationRestaurant(),
     };
   }
 }
---
+++ lib/src/pages/information_restaurant_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//   /informationRestaurant
+class InformationRestaurant extends StatefulWidget {
+
+  @override
+  _InformationRestaurantState createState() => _InformationRestaurantState();
+}
+
+class _InformationRestaurantState extends State<InformationRestaurant> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        
+      )
+    );
+  }
+}
