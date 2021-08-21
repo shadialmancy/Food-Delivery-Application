0ba281cf06e1c7f2ecafa27e9005e6e93b7906be
shadialmancy <shadi.almancy.sa@gmail.com>
2021-08-21 05:05:59 +0200

    payment page

--- lib/src/pages/payment_page.dart
+++ lib/src/pages/payment_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class PaymentPage extends StatefulWidget {
@@ -22,33 +21,41 @@ class _PaymentPageState extends State<PaymentPage> {
       ),
     );
   }
-}
 
-Widget column(BuildContext context) {
-  return Stack(
-    children : [
+
+  Widget column(BuildContext context) {
+    return Stack(children: [
       Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          const SizedBox(
+            height: 35,
+          ),
           row(),
           const SizedBox(
             height: 20,
           ),
           headertext("Metodos de pago"),
           Container(
-            padding: const EdgeInsets.all(10),
-            width: 340,
+            padding: const EdgeInsets.all(15),
             child: Column(
               children: [
-                textfield(),
+                cash(),
+                const Divider(
+                  thickness: 3,
+                  indent: 40,
+                ),
                 const SizedBox(
                   height: 10,
                 ),
-                passcode(),
+                creditCard(),
+                const Divider(
+                  thickness: 3,
+                  indent: 40,
+                ),
                 const SizedBox(
                   height: 15,
                 ),
-
               ],
             ),
           ),
@@ -60,134 +67,176 @@ Widget column(BuildContext context) {
           applyCredits()
         ],
       ),
-    ]
-  );
-}
+    ]);
+  }
 
-Widget row() {
-  return Row(
-    children: [closeButton(), headertext("Opciones de pago")],
-  );
-}
+  Widget row() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [closeButton(), headertext("Opciones de pago")],
+    );
+  }
 
-Widget closeButton() {
-  return Container(
-    child: Row(
-      children: [
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(FontAwesomeIcons.times),
-          iconSize: 50,
-        ),
-      ],
-    ),
-  );
-}
-
-Widget headertext(String text) {
-  return Container(
-    padding: const EdgeInsets.all(10),
-    child: Text(text,
-        style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w900)),
-  );
-}
-
-Widget textfield() {
-  return TextFormField(
-    decoration: const InputDecoration(
-      icon: Icon(
-        FontAwesomeIcons.moneyBillWave,
-      ),
-    ),
-  );
-}
-
-Widget passcode() {
-  return TextFormField(
-    keyboardType: TextInputType.phone,
-    obscureText: true,
-    decoration: const InputDecoration(
-      icon: Icon(
-        FontAwesomeIcons.solidCreditCard,
-      ),
-    ),
-  );
-}
-
-Widget paymentMethod() {
-  return InkWell(
-    onTap: () {},
-    child: Container(
+  Widget closeButton() {
+    return Container(
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          const Icon(
-            Icons.add,
-            size: 45,
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(FontAwesomeIcons.times),
+            iconSize: 50,
           ),
-          const SizedBox(
-            width: 10,
-          ),
-          const Text(
-            "Agregar metodo de pago",
-            style: TextStyle(fontSize: 20),
-          )
         ],
       ),
-    ),
-  );
-}
+    );
+  }
 
-bool isSwitched = false;
+  Widget headertext(String text) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: Text(text,
+          style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w900)),
+    );
+  }
 
-void toggleSwitch(bool isSwitched) {
-  isSwitched = !isSwitched;
-}
+  bool cashSelected = true;
+  bool creditSelected = false;
 
-Widget applyCredits() {
-  return Container(
-    padding: const EdgeInsets.all(10),
-    child: Row(
-      children: [
-        const Text(
-          "Aplicar creditos",
-          style: TextStyle(fontSize: 20),
+  Widget cash() {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          cashSelected = true;
+          creditSelected = false;
+        });
+      },
+      child: Container(
+        height: 50,
+        child: Row(
+          children: [
+            const Icon(
+              FontAwesomeIcons.moneyBillWave,
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            const Text(
+              "Efectivo",
+              style: TextStyle(fontSize: 20),
+            ),
+            const Spacer(
+              flex: 1,
+            ),
+            cashSelected ? const Icon(Icons.check) : Container()
+          ],
         ),
-        const SizedBox(
-          width: 130,
-        ),
-        switchButton()
-      ],
-    ),
-  );
-}
+      ),
+    );
+  }
 
-Widget switchButton() {
-  return Stack(
-    alignment: Alignment.center,
-    children: [
-      Container(
-        width: 34,
-        height:20,
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(
-            Radius.circular(10)
+  Widget creditCard() {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          cashSelected = false;
+          creditSelected = true;
+        });
+        print(cashSelected);
+        print(creditSelected);
+      },
+      child: Container(
+        height: 50,
+        child: Row(
+          children: [
+            const Icon(
+              FontAwesomeIcons.solidCreditCard,
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            const Text(
+              "*** **** *** 5880",
+              style: TextStyle(fontSize: 20),
+            ),
+            const Spacer(
+              flex: 1,
+            ),
+            creditSelected ? const Icon(Icons.check) : Container()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget paymentMethod() {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Icon(
+              Icons.add,
+              size: 45,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            const Text(
+              "Agregar metodo de pago",
+              style: TextStyle(fontSize: 20),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  bool isSwitched = false;
+
+  void toggleSwitch(bool isSwitched) {
+    isSwitched = !isSwitched;
+  }
+
+  Widget applyCredits() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            "Aplicar creditos",
+            style: TextStyle(fontSize: 20),
           ),
-          color: Colors.black,
+          switchButton()
+        ],
+      ),
+    );
+  }
+
+  Widget switchButton() {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          width: 34,
+          height: 20,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            color: Colors.black,
+          ),
         ),
-  ),
-       Transform.scale(
+        Transform.scale(
             scale: 0.5,
             child: Switch(
                 activeTrackColor: Colors.black,
                 activeColor: Colors.white,
                 inactiveTrackColor: Colors.black,
                 value: isSwitched,
-                onChanged: (value){
-                  isSwitched=value;
-                }
-            )
-        ),
-    ],
-  );
+                onChanged: (value) {
+                  isSwitched = value;
+                })),
+      ],
+    );
+  }
 }
